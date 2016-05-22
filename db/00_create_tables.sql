2ea95cf5389418af8d490fff47ac91e951775f57
Alexander Ose <alex@alexose.com>
2016-05-21 17:47:19 -0700

    prepare for upserting and other cool things

--- db/00_create_tables.sql
+++ db/00_create_tables.sql
@@ -3,14 +3,14 @@ CREATE TABLE heartbeats (
   pid varchar(20) NOT NULL,
   email varchar(255) NOT NULL,
   agent varchar(255) NOT NULL,
-  added TIMESTAMP DEFAULT NULL,
+  added TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP; 
   updated TIMESTAMP DEFAULT NULL
 );
 
 CREATE TABLE events (
   id bigserial PRIMARY key,
   heartbeat varchar(20) NOT NULL,
-  time TIMESTAMP NOT NULL,
+  time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP; 
   value varchar(20) DEFAULT NULL,
   min varchar(20) DEFAULT NULL,
   max varchar(20) DEFAULT NULL
