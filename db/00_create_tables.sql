071871df106c7d0918d72463559e9076a6312cad
Alexander Ose <alex@alexose.com>
2016-05-21 18:02:20 -0700

    working upserts

--- db/00_create_tables.sql
+++ db/00_create_tables.sql
@@ -1,16 +1,16 @@
 CREATE TABLE heartbeats (
   id bigserial PRIMARY key,
-  pid varchar(20) NOT NULL,
+  pid varchar(255) NOT NULL UNIQUE,
   email varchar(255) NOT NULL,
   agent varchar(255) NOT NULL,
-  added TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP; 
+  added TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   updated TIMESTAMP DEFAULT NULL
 );
 
 CREATE TABLE events (
   id bigserial PRIMARY key,
   heartbeat varchar(20) NOT NULL,
-  time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP; 
+  time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   value varchar(20) DEFAULT NULL,
   min varchar(20) DEFAULT NULL,
   max varchar(20) DEFAULT NULL
